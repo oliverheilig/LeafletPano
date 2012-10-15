cc26ee3eee2190d864660282f803b23e63302828
oliverheilig <oliver.heilig@yahoo.de>
2012-10-15 21:44:54 +0200

    initial version

---
+++ MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace LeafletPano
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(463, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(445, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 47);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.startButton);
+            this.Name = "MainForm";
+            this.Text = "LeafletPano";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LeafletPano
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            textBox1.Text = Application.StartupPath + "\\DemoData\\Rothenburg.jpg";
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            CreateTiles(textBox1.Text);
+        }
+
+        public void CreateTiles(string imageFile)
+        {
+            string directoryName = System.IO.Path.GetDirectoryName(imageFile);
+            string imageName = System.IO.Path.GetFileNameWithoutExtension(imageFile);
+
+            System.IO.Directory.CreateDirectory(directoryName + "/" + imageName);
+
+            var srcImg = Image.FromFile(imageFile);
+
+            var width = srcImg.Width;
+            var height = srcImg.Height;
+            var size = Math.Max(width, height);
+            var numTiles = size / 256;
+            var maxLevel = (int)Math.Ceiling(Math.Log(numTiles, 2));
+
+            for (int level = 0; level <= maxLevel; level++)
+            {
+                int logTileSize = 256 * (1 << (maxLevel - level));
+                int numTilesX = (width % logTileSize == 0) ? width / logTileSize : width / logTileSize + 1;
+                int numTilesY = (height % logTileSize == 0) ? height / logTileSize : height / logTileSize + 1;
+
+                for (int tx = 0; tx < numTilesX; tx++)
+                    for (int ty = 0; ty < numTilesY; ty++)
+                    {
+                        using (var tileImg = new Bitmap(256, 256))
+                        using (var graphcis = Graphics.FromImage(tileImg))
+                        {
+                            graphcis.FillRectangle(Brushes.White, 0, 0, 256, 256);
+                            graphcis.DrawImage(srcImg, new Rectangle(0, 0, 256, 256),
+                                new Rectangle(tx * logTileSize, ty * logTileSize, logTileSize, logTileSize),
+                                GraphicsUnit.Pixel);
+
+                            tileImg.Save(string.Format("{0}/{1}/{2}-{3}-{4}.jpg", directoryName, imageName, level, tx, ty),
+                                System.Drawing.Imaging.ImageFormat.Jpeg);
+                        }
+                    }
+            }
+
+            var reader = new System.IO.StreamReader(Application.StartupPath + "\\Template.html");
+            var template = reader.ReadToEnd();
+            reader.Close();
+
+            template = template.Replace("<<image>>", imageName);
+            template = template.Replace("<<width>>", width.ToString());
+            template = template.Replace("<<height>>", height.ToString());
+            template = template.Replace("<<maxLevel>>", maxLevel.ToString());
+        
+            string htmlFile = directoryName + "/" + imageName + ".html";
+            var writer = new System.IO.StreamWriter(htmlFile);
+            writer.Write(template);
+            writer.Close();
+
+            System.Diagnostics.Process.Start(htmlFile);
+        }
+    }
+}
